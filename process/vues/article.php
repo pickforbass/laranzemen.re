d8f0828a25c2624689a2ce34d201403c3b4aad77
pickforbass <56023752+pickforbass@users.noreply.github.com>
2020-05-01 15:48:23 +0200

    Creating vues

--- process/vues/article.php
+++ process/vues/article.php
@@ -1,3 +1,15 @@
 <?php
+require "../elements/header.php";
+global $conn;
+$id_article = $_GET['id'];
 
+$article = $conn->query("SELECT * FROM article WHERE id = $id_article");
+?>
+
+<main class="container-fluid">
+</main>
+
+<?php
+require "../elements/footer.php";
+?>
 
--- process/vues/recipe.php
+++ process/vues/recipe.php
@@ -1,3 +1,16 @@
 <?php
+require "../elements/header.php";
+global $conn;
+$id_recipe = $_GET['id'];
+
+$recipe = $conn->query("SELECT * FROM recipe WHERE id = $id_recipe");
+?>
+
+<main class="container-fluid">
 
 
+</main>
+
+<?php
+require "../elements/footer.php";
+?>
