e5a3c5b1e20cc0c897918a3d2c8681c552997e01
pickforbass <56023752+pickforbass@users.noreply.github.com>
2020-05-06 16:38:00 +0200

    footer.php cleaned add fontawesome cdn

--- process/parts/footer.php
+++ process/parts/footer.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <footer>
-    <div id="sn-link"> INSTA - FB - PINTEREST</div>
-    <p>Made with sweat tears and self-confidence by QF - 2020</p>
+    <div id="sn-link"> <i class="fab fa-instagram"></i> <i class="fab fa-facebook-f"></i> <i class="fab fa-pinterest-p"></i></div>
+    <p class="text-right"><small>Made with sweat tears and self-confidence by QF - 2020</small></p>
 </footer>
 
--- process/parts/header.php
+++ process/parts/header.php
@@ -11,6 +11,7 @@ global $conn;
     <meta name="viewport" content="width=device-width, user-scalable=0">
     <title>l'aranzemen</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1e527daaa5.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../styles/styles.css">
 </head>
 <body>
